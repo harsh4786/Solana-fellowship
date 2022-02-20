01633d40655f8f99fe0cd95e3131c8a9768b9b61
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2022-02-20 18:28:49 +0530

    Update index.js

--- roulette/index.js
+++ roulette/index.js
@@ -1,1 +1,164 @@
 
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const { Keypair } = require('@solana/web3.js');
+
+const init = () => {
+    console.log(
+        chalk.green(
+        figlet.textSync("SOL Stake", {
+            font: "Standard",
+            horizontalLayout: "default",
+            verticalLayout: "default"
+        })
+        )
+    );
+    console.log(chalk.yellow`The max bidding amount is 2.5 SOL here`);
+};
+
+
+const userSecretKey = [
+ [124,113,15,233,127,0,65,120,147,92,162,109,209,34,201,92,159,113,74,25,52,112,17,240,87,238,69,17,137,25,131,128,196,118,176,73,164,181,128,195,37,186,252,152,168,178,156,123,110,163,224,150,165,109,93,130,46,229,23,141,67,116,140,3]
+]
+
+const userWallet = Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
+
+
+//Treasury
+const secretKey = [
+    111, 188,  76, 169,  30, 105, 254,  33, 228,  66,  56,
+    215,   9,  37,  51, 188, 188, 188,  20, 224, 228, 115,
+     17, 163, 151, 105, 113, 251, 105, 177,  28, 157, 125,
+    202, 195, 203, 253, 137,  26, 209,   7,   2,  66, 193,
+     76, 241, 203, 168, 213,   5, 226,  11, 142,  44, 125,
+    191, 167, 172, 166, 207, 176, 137, 210,  27
+]
+
+const treasuryWallet = Keypair.fromSecretKey(Uint8Array.from(secretKey));
+
+
+const web3 = require("@solana/web3.js");
+
+const getWalletBalance=async (pubk)=>{
+    try{
+        const connection = new web3.Connection(web3.clusterApiUrl("devnet"),"confirmed");
+        const balance = await connection.getBalance(new web3.PublicKey(pubk));
+        return balance/web3.LAMPORTS_PER_SOL;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const transferSOL = async (from,to,transferAmt)=>{
+    try{
+        const connection=new web3.Connection(web3.clusterApiUrl("devnet"),"confirmed");
+        const transaction=new web3.Transaction().add(
+            web3.SystemProgram.transfer({
+                fromPubkey:new web3.PublicKey(from.publicKey.toString()),
+                toPubkey:new web3.PublicKey(to.publicKey.toString()),
+                lamports:transferAmt*web3.LAMPORTS_PER_SOL
+            })
+        )
+        const signature = await web3.sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [from]
+        )
+        return signature;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const airDropSol = async (wallet,transferAmt)=>{
+    try{
+        const connection = new web3.Connection(web3.clusterApiUrl("devnet"),"confirmed");
+        // const walletKeyPair=await web3.Keypair.fromSecretKey(Uint8Array.from())
+        const fromAirDropSignature=await connection.requestAirdrop(new web3.PublicKey(wallet.publicKey.toString()),transferAmt*web3.LAMPORTS_PER_SOL);
+        await connection.confirmTransaction(fromAirDropSignature);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const askQuestions = () => {
+    const questions = [
+        {
+            name: "SOL",
+            type: "number",
+            message: "What is the amount of SOL you want to stake?",
+        },
+        {
+            type: "rawlist",
+            name: "RATIO",
+            message: "What is the ratio of your staking?",
+            choices: ["1:1.25", "1:1.5", "1.75", "1:2"],
+            filter: function(val) {
+                const stakeFactor = val.split(":")[1];
+                return stakeFactor;
+            },
+        },
+        {
+            type:"number",
+            name:"RANDOM",
+            message:"Guess a random number from 1 to 5 (both 1, 5 included)",
+            when:async (val)=>{
+                if(parseFloat(totalAmtToBePaid(val.SOL))>5){
+                    console.log(chalk.red`You have violated the max stake limit. Stake with smaller amount.`)
+                    return false;
+                }else{
+                    // console.log("In when")
+                    console.log(`You need to pay ${chalk.green`${totalAmtToBePaid(val.SOL)}`} to move forward`)
+                    const userBalance=await getWalletBalance(userWallet.publicKey.toString())
+                    if(userBalance<totalAmtToBePaid(val.SOL)){
+                        console.log(chalk.red`You don't have enough balance in your wallet`);
+                        return false;
+                    }else{
+                        console.log(chalk.green`You will get ${getReturnAmount(val.SOL,parseFloat(val.RATIO))} if guessing the number correctly`)
+                        return true;    
+                    }
+                }
+            },
+        }
+    ];
+    return inquirer.prompt(questions);
+};
+
+
+const gameExecution = async () =>{
+    init();
+    const generateRandomNumber = randomNumber(1,5);
+    // console.log("Generated number",generateRandomNumber);
+    const answers = await askQuestions();
+    if(answers.RANDOM){
+        const paymentSignature=await transferSOL(userWallet,treasuryWallet,totalAmtToBePaid(answers.SOL))
+        console.log(`Signature of payment for playing the game`,chalk.green`${paymentSignature}`);
+        if(answers.RANDOM === generateRandomNumber){
+            //AirDrop Winning Amount
+            await airDropSol(treasuryWallet,getReturnAmount(answers.SOL,parseFloat(answers.RATIO)));
+            //guess is successful
+            const prizeSignature=await transferSOL(treasuryWallet,userWallet,getReturnAmount(answers.SOL,parseFloat(answers.RATIO)))
+            console.log(chalk.green`Your guess is absolutely correct`);
+            console.log(`Here is the price signature `,chalk.green`${prizeSignature}`);
+        }else{
+            //better luck next time
+            console.log(chalk.yellowBright`Better luck next time`)
+        }
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function totalAmtToBePaid(investment){
+    // we're keeping 5% as the participation fee 
+    return investment + 0.05*investment;
+}
+
+function getReturnAmount(investment, stakeFactor){
+    return investment*stakeFactor;
+}
+
+gameExecution()
